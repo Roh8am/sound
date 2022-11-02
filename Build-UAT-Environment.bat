77489133b5c9622c4eed4397667f1c3b90032db6
Roh8am <111047435+Roh8am@users.noreply.github.com>
2022-11-02 18:11:44 +0530

    Delete Build-UAT-Environment.bat

--- Build-UAT-Environment.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-DockerBuild-UAT.bat E:\frontiergit\release\ff708-Frontier-FrontierWallBoardMobile\UAT \\10.10.180.173\C$ \\10.10.180.104\CICDDeployProjectBackup\ff708-Frontier-FrontierWallBoardMobile\UAT
